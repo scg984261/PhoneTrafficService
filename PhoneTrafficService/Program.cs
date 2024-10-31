2feb6b0f17f38e78b2f954a647b54d3715a40aba
Scott Gillen <scottgillen@hotmail.com>
2024-10-31 21:53:40 +0000

    Reading values from incoming csv

--- PhoneTrafficService/Program.cs
+++ PhoneTrafficService/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Configuration;
+using System.IO;
+using System.Linq;
 using log4net;
 using log4net.Config;
 
@@ -19,11 +21,30 @@ namespace PhoneTrafficService
         {   
             log.Info("Start of Application.");
             log.Info($"File location read from config: {incomingFileLocation}");
+
+            string[] lines = File.ReadAllLines(incomingFileLocation).Skip(1).ToArray();
+
+            string ddiNumber;
+            string numberOfCalls;
+
+            foreach (string line in lines)
+            {
+                // N.B. It seems there are two formats of INCOMING.CSV now - with one having total number of calls in one column,
+                // and the old one had incoming and outgoing calls in separate columns.
+                string[] columns = line.Split(',');
+                ddiNumber = columns[0];
+                numberOfCalls = columns[1];
+            }
         }
 
         public static string GetIncomingFilePathFromConfig()
         {
             return ConfigurationManager.AppSettings.Get("IncomingFilePath");
         }
+
+        // public static string[] GetFileContentsSkipFirstLine(string fileLocation)
+        // {
+        //     return File.ReadAllLines(fileLocation).Skip(1).ToArray();
+        // }
     }
 }
