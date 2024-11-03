43647d81727b0ca32c2764d4fd94104f39ab4265
Scott Gillen <scottgillen@hotmail.com>
2024-11-03 11:39:48 +0000

    Added logic to determine phone number traffic and write to spreadsheet

--- PhoneTrafficService/Program.cs
+++ PhoneTrafficService/Program.cs
@@ -1,8 +1,12 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using log4net;
 using log4net.Config;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
 
 namespace PhoneTrafficService
 {
@@ -26,6 +30,8 @@ namespace PhoneTrafficService
 
             string ddiNumber;
             string numberOfCalls;
+            int numberOfCallsInt;
+            Dictionary<string, int> incomingCallsDictionary = new Dictionary<string, int>();
 
             foreach (string line in lines)
             {
@@ -34,6 +40,72 @@ namespace PhoneTrafficService
                 string[] columns = line.Split(',');
                 ddiNumber = columns[0];
                 numberOfCalls = columns[1];
+
+                if (int.TryParse(numberOfCalls, out numberOfCallsInt))
+                {
+                    incomingCallsDictionary.Add(ddiNumber, numberOfCallsInt);
+                }
+                else
+                {
+                    log.Error($"Error occurred attempting to convert number of calls: {numberOfCalls} to an integer type!");
+                    incomingCallsDictionary.Add(ddiNumber, 0);
+                }
+            }
+
+            string spreadsheetFileName = ConfigurationManager.AppSettings.Get("PhoneNumbersAllocatedSpreadsheet");
+
+            HSSFWorkbook hssfwb;
+
+            using (FileStream fileStream = new FileStream(spreadsheetFileName, FileMode.Open, FileAccess.Read))
+            {
+                hssfwb = new HSSFWorkbook(fileStream);
+                fileStream.Close();
+            }
+
+            ISheet sheet = hssfwb.GetSheetAt(0);
+
+            int lastRowNumber = sheet.LastRowNum;
+
+            // Start from cell 2, not top row.
+            for (int i = 1; i < lastRowNumber; i++)
+            {
+                IRow row = sheet.GetRow(i);
+                ICell cellE = row.CreateCell(4);
+
+                string phoneNumbersAllocatedDdiNumber;
+
+                try
+                {
+                    phoneNumbersAllocatedDdiNumber = row.GetCell(1).StringCellValue;
+                }
+                catch (Exception exception)
+                {
+                    log.Error(exception);
+                    continue;
+                }
+
+                string phoneNumbersAllocatedDdiNumberLastTenDigits = phoneNumbersAllocatedDdiNumber.Substring(Math.Max(0, phoneNumbersAllocatedDdiNumber.Length - 10));
+
+                int numberOfCallsNew;
+
+                if (incomingCallsDictionary.TryGetValue(phoneNumbersAllocatedDdiNumberLastTenDigits, out numberOfCallsNew))
+                {
+                    cellE.SetCellValue(numberOfCallsNew);
+                }
+                else if (phoneNumbersAllocatedDdiNumberLastTenDigits.Length > 0)
+                {
+                    cellE.SetCellValue(0);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            using (FileStream file = new FileStream(spreadsheetFileName, FileMode.Open, FileAccess.Write))
+            {
+                hssfwb.Write(file);
+                file.Close();
             }
         }
 
@@ -41,10 +113,5 @@ namespace PhoneTrafficService
         {
             return ConfigurationManager.AppSettings.Get("IncomingFilePath");
         }
-
-        // public static string[] GetFileContentsSkipFirstLine(string fileLocation)
-        // {
-        //     return File.ReadAllLines(fileLocation).Skip(1).ToArray();
-        // }
     }
 }
