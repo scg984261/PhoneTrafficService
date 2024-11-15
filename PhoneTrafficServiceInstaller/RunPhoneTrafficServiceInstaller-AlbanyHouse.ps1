3f6c384a870c16a8c61b7b41493028c26547cd38
Scott Gillen <scottgillen@hotmail.com>
2024-11-15 22:51:46 +0000

    Install and uninstall PowerShell scripts for PhoneTrafficService. Scripts are copied to bin directory when building the installer.

---
+++ PhoneTrafficServiceInstaller/RunPhoneTrafficServiceInstaller-AlbanyHouse.ps1
@@ -0,0 +1,9 @@
+﻿# Run the MSI Installer script.
+msiexec.exe /i PhoneTrafficService-Debug-x86-2.0.0.8.msi SP_RUNMODE="ALBANY_HOUSE" SP_INCOMING_PATH="C:\ELEPHANT\AHINCOMING.CSV" SP_SPREADSHEET_PATH=`"C:\PhoneNumbersAllocated\AH Phone Numbers Allocated.xls`" /quiet /passive /norestart
+
+# Create the Scheduled Task to run automatically.
+$Action = New-ScheduledTaskAction -Execute "C:\PhoneTrafficService\PhoneTrafficService.exe" -
+$Trigger = New-ScheduledTaskTrigger -Daily -At '2:00 AM'
+$Settings = New-ScheduledTaskSettingsSet
+$Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
+Register-ScheduledTask -TaskName "PhoneTrafficService" -InputObject $Task
---
+++ PhoneTrafficServiceInstaller/UninstallPhoneTrafficService.ps1
@@ -0,0 +1,5 @@
+﻿# Uninstall PhoneTrafficService
+msiexec.exe /x PhoneTrafficService-Debug-x86-2.0.0.8.msi /quiet /passive /norestart
+
+# Delete the scheduled task
+Unregister-ScheduledTask -TaskName "PhoneTrafficService" -Confirm:$false
