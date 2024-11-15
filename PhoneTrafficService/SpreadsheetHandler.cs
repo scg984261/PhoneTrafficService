07a3f85f875a2401a395cddb012f9d59bf1d8131
Scott Gillen <scottgillen@hotmail.com>
2024-11-15 22:54:41 +0000

    Changed log message in SpreadsheetHandler to log the DDI number when a match is found.

--- PhoneTrafficService/SpreadsheetHandler.cs
+++ PhoneTrafficService/SpreadsheetHandler.cs
@@ -104,7 +104,7 @@ namespace PhoneTrafficService
 
             if (dictionary.TryGetValue(ddiNumber, out numberOfCalls))
             {
-                log.Debug($"Setting traffic column of row number: {cell.Row.RowNum + 1} to: {numberOfCalls}.");
+                log.Debug($"Match found for DDI Number: {ddiNumber}. Setting traffic column of row number: {cell.Row.RowNum + 1} to: {numberOfCalls}.");
                 cell.SetCellValue(numberOfCalls);
             }
             else
