1be9ba2964e251f7e169f6cf3cbfb21deb49da84
Scott Gillen <scottgillen@hotmail.com>
2024-11-19 22:09:54 +0000

    Rolling Phone Traffic Service Main Project back to v3.5

--- PhoneTrafficService/Properties/AssemblyInfo.cs
+++ PhoneTrafficService/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PhoneTrafficService")]
+[assembly: AssemblyTitle("ConsoleApp1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PhoneTrafficService")]
+[assembly: AssemblyProduct("ConsoleApp1")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("44917dad-c345-4f64-83ea-571b2647a0e9")]
+[assembly: Guid("4a68b4e9-4aa6-41d5-94d0-bdf66b77ad66")]
 
 // Version information for an assembly consists of the following four values:
 //
